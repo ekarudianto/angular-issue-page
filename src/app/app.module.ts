ba1ffccccf3ebb4271d7b83c15aadd68428a8175
Eka Rudianto <ekarudianto@yahoo.com>
2018-03-26 02:48:55 +0800

    Update app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,38 @@
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { MarkdownModule } from 'angular2-markdown';
 import { NgModule } from '@angular/core';
 
+import { CommentService } from './services/comment.service';
+import { IssueService } from './services/issue.service';
+
 import { AppComponent } from './app.component';
-import { IssuesComponent } from './issues/issues.component';
-import { IssueComponent } from './issue/issue.component';
-
-import { AppRoutingModule } from './app-routing.module';
-
-import { IssueService } from './issue.service';
+import { CommentComponent } from './components/issue-detail/comment/comment.component';
+import { IconComponent } from './components/icon/icon.component';
+import { IssueComponent } from './components/issues/issue/issue.component';
+import { IssueDetailComponent } from './components/issue-detail/issue-detail.component';
+import { IssuesComponent } from './components/issues/issues.component';
+import { LabelComponent } from './components/label/label.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     IssuesComponent,
-    IssueComponent
+    IssueComponent,
+    IconComponent,
+    LabelComponent,
+    IssueDetailComponent,
+    CommentComponent,
   ],
   imports: [
     AppRoutingModule,
-    BrowserModule
+    HttpClientModule,
+    BrowserModule,
+    MarkdownModule.forRoot(),
   ],
-  providers: [ IssueService ],
+  providers: [ CommentService, IssueService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
